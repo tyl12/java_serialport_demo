72bc6843c2d05f423d984485d37802f34ac0d0eb
Teng Yiliang <ylteng@outlook.com>
2018-04-12 12:19:10 +0800

    udpate. ok.
    Signed-off-by: Teng Yiliang <ylteng@outlook.com>

--- a.sh
+++ a.sh
@@ -1,2 +1,9 @@
 #java com.xiaomeng.rxtx
-java com.xiaomeng.rxtx.Example -Dorg.eclipse.jdt.launching.CLASSPATH_ATTR_LIBRARY_PATH_ENTRY=/home/tony/rxtx/lib
+#java com.xiaomeng.rxtx.Example -Dorg.eclipse.jdt.launching.CLASSPATH_ATTR_LIBRARY_PATH_ENTRY=/home/tony/rxtx/lib
+
+
+#java -cp rxtx-0.0.1-SNAPSHOT.jar:lib/*  com.xiaomeng.rxtx.Example -Djava.library.path=lib
+
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
+java -cp rxtx-0.0.1-SNAPSHOT.jar:lib/*  com.xiaomeng.rxtx.Example
